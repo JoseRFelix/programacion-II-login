54f2ea02657106afb89dc2f48a31df7b99190339
josegr88 <josegr88dom@gmail.com>
2019-05-30 00:35:04 -0400

    Termine de dar formato a como serán los perfiles, me falta completar el actual y 3 mas

---
+++ app/src/main/java/com/programacion_ii_login/ui/perfiles/mayormonograma.java
@@ -0,0 +1,15 @@
+package com.programacion_ii_login.ui.perfiles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.programacion_ii_login.R;
+
+public class mayormonograma extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mayormonograma);
+    }
+}
